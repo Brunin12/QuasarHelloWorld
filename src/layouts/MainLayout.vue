8d593f510d9938151ca525c204b0bc45c0a19e6e
Brunin12 <brunoparreira77@gmail.com>
2023-12-08 09:44:47 -0300

    initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,80 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Olá Mundo
+        </q-toolbar-title>
+
+        <div>Bruno Soft</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Sobre mim
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import EssentialLink from 'components/EssentialLink.vue';
+
+const linksList = [
+  {
+    title: 'Github',
+    caption: 'github.com/Brunin12',
+    icon: 'code',
+    link: 'https://github.com/Brunin12'
+  },
+];
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+});
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-banner dense class="bg-primary text-white">
+      Olá Mundo!!!!!!!!!!!
+      <template v-slot:action>
+        <q-btn flat color="white" label="Olá" />
+      </template>
+    </q-banner>
+  </div>
+</template>
